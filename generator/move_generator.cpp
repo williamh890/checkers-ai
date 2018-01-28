c441c36147bc0f8951a48784bbbb0c1e8beefd64
William Horn <wbhorn@alaska.edu>
2018-01-28 01:42:52 -0900

    Function to get the grid position from the space.

--- generator/move_generator.cpp
+++ generator/move_generator.cpp
@@ -6,6 +6,9 @@ using std::cout;
 using std::endl;
 #include <string>
 using std::string;
+#include <cstddef>
+using std::pair;
+using std::make_pair;
 
 class MoveGenerator {
     private:
@@ -70,8 +73,55 @@ CheckersGame getCheckersGame() {
     return CheckersGame(red, black);
 }
 
+/*
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    | X | r |   | r |   | r |   | r |     | X | 0 |   | 1 |   | 2 |   | 3 |
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    | r |   | r |   | r |   | r |   |     | 4 |   | 5 |   | 6 |   | 7 |   |
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    |   | r |   | r |   | r |   |   |     |   | 8 |   | 9 |   | 10|   | 11|
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    |   |   |   |   |   |   |   |   |     | 12|   | 13|   | 14|   | 15|   |
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    |   |   |   |   |   |   |   |   |     |   | 16|   | 17|   | 18|   | 19|
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    | b |   | b |   | b |   | b |   |     | 20|   | 21|   | 22|   | 23|   |
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    |   | b |   | b |   | b |   | b |     |   | 24|   | 25|   | 26|   | 27|
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+    | b |   | b |   | b |   | b | X |     | 28|   | 29|   | 30|   | 31| X |
+    +---+---+---+---+---+---+---+---+     +---+---+---+---+---+---+---+---+
+
+
+*/
+struct Position {
+    int row;
+    int col;
+    Position(int r, int c): row(r), col(c) {}
+};
+
+Position spaceToGridSquare(int space) {
+    auto row = space / 4;
+    auto col = space % 4;
+
+    auto offset = (row % 2) ? 0 : 1;
+
+    return Position(row, (2 * col) + offset);
+}
+
+string boardToString(const vector<Piece> pieces) {
+    return "";
+}
+
 int main() {
-    auto game = getCheckersGame();
+    // auto game = getCheckersGame();
+    for (auto i = 0; i < 32; ++i) {
+        auto space = spaceToGridSquare(i);
+        cout << "Space (" << i << ") = "
+            << "r: " << space.row << ", c: " << space.col
+            << endl;
+    }
+
     return 0;
 }
 
