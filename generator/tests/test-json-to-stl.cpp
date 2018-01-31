38f090b71417ca31b98d9eb72ae8d2a012592887
William Horn <wbhorn@alaska.edu>
2018-01-30 22:30:36 -0900

    Added tests to makefile and catch.hpp to `make install`
    make tests to build tests
    make tests-run to run them.

--- generator/tests/test-json-to-stl.cpp
+++ generator/tests/test-json-to-stl.cpp
@@ -4,5 +4,5 @@ using ai::loadMoveTableFrom;
 #include "catch.hpp"
 
 TEST_CASE("testing loading move table from json."){
-    auto json = loadMoveTableFrom("test.json");
+    auto json = loadMoveTableFrom("tests/test.json");
 }
