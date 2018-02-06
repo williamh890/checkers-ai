b0d6b59c3bce2301b66b40dec3b17eeb5f33eef5
William Horn <wbhorn@alaska.edu>
2018-02-06 00:35:21 -0900

    Changing class name to be same as filename.
    Also putting it in namespace ai so there must be a using statement
    after the file is included. This is to anontate what files
    function/classes are coming from.

--- generator/network.cpp
+++ generator/network.cpp
@@ -1,75 +1,81 @@
 #include "headers\network.h"
+using ai::Network;
+
 #include <vector>
 using std::vector;
 using std::swap;
 
-AI_Network::AI_Network(int inputID): _ID(inputID) {
+Network::Network(int inputID): _ID(inputID) {
 	loadWeightsFromFile();
 };
 
-AI_Network::AI_Network(const vector<int> & inputdimensions) {
+Network::Network(const vector<int> & inputdimensions) {
 	setupNetwork(inputdimensions);
 }
 
-AI_Network::~AI_Network() {
+Network::~Network() {
 	if (_gameCompleted) {
 		saveWeightsToFile();
 	}
 };
 
-void AI_Network::setupNetwork() {
+void Network::setupNetwork() {
 
 }
 
-void AI_Network::setupNetwork(const vector<int>& dimensions) {
+void Network::setupNetwork(const vector<int>& dimensions) {
 
 };
 
-void AI_Network::loadWeightsFromFile() { //*** TODO ***
+void Network::loadWeightsFromFile() { //*** TODO ***
 	// I intend to implement this using boost::serialization
 };
 
-void AI_Network::saveWeightsToFile() { 	//***TODO***
+void Network::saveWeightsToFile() { 	//***TODO***
 	// I intend to implement this using boost::serialization
 };
 
-double AI_Network::evaluateBoard(vector<char> inputBoard) const { 	//***TODO***
+double Network::evaluateBoard(vector<char> inputBoard) const { 	//***TODO***
 	double dummy = 0;
 	return dummy;
 };
-void AI_Network::adjustPerformance(int resultFromGame) {
+
+void Network::adjustPerformance(int resultFromGame) {
 	_performance += resultFromGame;
 	_gameCompleted = true;
 };
 
-int AI_Network::getPerformance() const {
+int Network::getPerformance() const {
 	return _performance;
 }
-void AI_Network::resetPerformance() {
+
+void Network::resetPerformance() {
 	_performance = 0;
 };
-vector<vector<double>> & AI_Network::evolve() const { 	// *** TODO ***
+
+vector<vector<double>> & Network::evolve() const { 	// *** TODO ***
 	vector<weightsInLayer> dummy;
 	return dummy;
 };
-void AI_Network::replaceWithEvolution(vector<weightsInLayer> & inputNetwork) {
+
+void Network::replaceWithEvolution(vector<weightsInLayer> & inputNetwork) {
 	swap(_layers, inputNetwork);
 };
 
-bool operator<(const AI_Network & lhs, const AI_Network & rhs) {
+bool operator<(const Network & lhs, const Network & rhs) {
 	if (lhs.getPerformance() < rhs.getPerformance())
 		return true;
 	return false;
 }
 
-bool operator>(const AI_Network & lhs, const AI_Network & rhs) {
+bool operator>(const Network & lhs, const Network & rhs) {
 	return rhs < lhs;
 }
 
-bool operator<=(const AI_Network & lhs, const AI_Network & rhs) {
+bool operator<=(const Network & lhs, const Network & rhs) {
 	return !(lhs > rhs);
 }
 
-bool operator>=(const AI_Network & lhs, const AI_Network & rhs) {
+bool operator>=(const Network & lhs, const Network & rhs) {
 	return !(lhs < rhs);
 }
