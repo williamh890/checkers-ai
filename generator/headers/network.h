9fb88bc1f677aad14372a80842991be9384288dd
Kai Davids Schell <kdavidsschell@alaska.edu>
2018-02-05 20:43:21 -0900

    just added a "using std::vector" to network.h and cleaned a bit

--- generator/headers/network.h
+++ generator/headers/network.h
@@ -1,32 +1,33 @@
 #ifndef NETWORK_H_INCLUDED
 #define NETWORK_H_INCLUDED
 #include <vector>
+using std::vector;
 
 class AI_Network {
 public:
-	using weightsInLayer = std::vector<double>;
+	using weightsInLayer = vector<double>;
 
 	AI_Network(int);
-	AI_Network(const std::vector<int>&);
+	AI_Network(const vector<int>&);
 	~AI_Network();
 
 	void setupNetwork();
-	void setupNetwork(const std::vector<int> &);
+	void setupNetwork(const vector<int> &);
 
 	void loadWeightsFromFile(); // *** TODO ***
 	void saveWeightsToFile();	// *** TODO ***
 	
-	double evaluateBoard(std::vector<char>) const; // *** TODO ***
+	double evaluateBoard(vector<char>) const; // *** TODO ***
 	void adjustPerformance(int result);
 	int getPerformance() const;
 	void resetPerformance();
 
-	std::vector<weightsInLayer> & evolve() const; // *** TODO ***
-	void replaceWithEvolution(std::vector<weightsInLayer> &);
+	vector<weightsInLayer> & evolve() const; // *** TODO ***
+	void replaceWithEvolution(vector<weightsInLayer> &);
 
 private:
 	int _ID;
-	std::vector<weightsInLayer> _layers;
+	vector<weightsInLayer> _layers;
 	int _performance;
 	bool _gameCompleted = false;
 }; // end class AI_Network
