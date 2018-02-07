3d0532cfaed03833764b775c209c4a0b9b5b1dda
Duane Shaffer <dlshaffer@alaska.edu>
2018-02-06 16:08:28 -0900

    Implemented serialization. Successfully created a file and saves into it. Users need to get the boost library.

--- generator/headers/network.h
+++ generator/headers/network.h
@@ -1,42 +1,61 @@
 #ifndef NETWORK_H_INCLUDED
 #define NETWORK_H_INCLUDED
-
 #include <vector>
 
-namespace ai {
-    class Network {
-        public:
-            using weightsInLayer = std::vector<double>;
+#include "boost\archive\basic_text_iarchive.hpp"
+#include "boost\archive\basic_text_oarchive.hpp"
+#include "boost\serialization\vector.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+namespace AI {
+	class Network {
+	public:
+		using weightsInLayer = std::vector<double>;
 
-            Network(int);
-            Network(const std::vector<int>&);
-            ~Network();
+		Network(int);
+		Network(const std::vector<int>&);
+		~Network();
 
-            void setupNetwork();
-            void setupNetwork(const std::vector<int> &);
+		double evaluateBoard(std::vector<char>) const; // *** TODO ***
+		void adjustPerformance(int result);
+		int getPerformance() const;
+		void resetPerformance();
 
-            void loadWeightsFromFile(); // *** TODO ***
-            void saveWeightsToFile();	// *** TODO ***
+		std::vector<weightsInLayer> evolve() const; // *** TODO *** Not required for Project 2
+		void replaceWithEvolution(const Network &);
 
-            double evaluateBoard(std::vector<char>) const; // *** TODO ***
-            void adjustPerformance(int result);
-            int getPerformance() const;
-            void resetPerformance();
+	private:
+		int _ID; // Might not be needed
+		std::vector<weightsInLayer> _layers;
+		int _performance;
+		bool _gameCompleted = true;
 
-            std::vector<weightsInLayer> & evolve() const; // *** TODO ***
-            void replaceWithEvolution(std::vector<weightsInLayer> &);
+		friend class boost::serialization::access;
+		template <typename Archive>
+		void serialize(Archive &ar, const unsigned int version) {
+			ar & _ID;
+			ar & _layers;
+			ar & _performance;
+		}
 
-        private:
-            int _ID;
-            std::vector<weightsInLayer> _layers;
-            int _performance;
-            bool _gameCompleted = false;
-    }; // end class Network
+	}; // end class AI_Network
 
-    bool operator< (const Network &lhs, const Network &rhs);
-    bool operator> (const Network &lhs, const Network &rhs);
-    bool operator<= (const Network &lhs, const Network &rhs);
-    bool operator>= (const Network &lhs, const Network &rhs);
+
+	// Global operators to allow sorting of networks based on their performance
+	bool operator< (const Network &lhs, const Network &rhs);
+	bool operator> (const Network &lhs, const Network &rhs);
+	bool operator<= (const Network &lhs, const Network &rhs);
+	bool operator>= (const Network &lhs, const Network &rhs);
+
+
+	void setupNetwork();
+	void setupNetwork(const std::vector<int> &);
+
+	// Functions to handle saving and loading of networks utilizing boost serialization
+	std::string idToFilename(int);
+	void saveNetwork(int, Network &);
+	bool loadNetwork(int, Network &);
+
 }
-
 #endif // NETWORK_H_INCLUDED
--- generator/network.cpp
+++ generator/network.cpp
@@ -1,12 +1,25 @@
 #include "headers\network.h"
-using ai::Network;
-
+using AI::Network;
 #include <vector>
 using std::vector;
-using std::swap;
+
+#include "boost\archive\text_iarchive.hpp"
+#include "boost\archive\text_oarchive.hpp"
+using boost::archive::text_iarchive;
+using boost::archive::text_oarchive;
+#include "boost\serialization\vector.hpp"
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+#include <iostream>
+#include <string>
+using std::string;
+using std::to_string;
+
+
 
 Network::Network(int inputID): _ID(inputID) {
-	loadWeightsFromFile();
+	loadNetwork(_ID, *this);
 };
 
 Network::Network(const vector<int> & inputdimensions) {
@@ -15,31 +28,14 @@ Network::Network(const vector<int> & inputdimensions) {
 
 Network::~Network() {
 	if (_gameCompleted) {
-		saveWeightsToFile();
+		saveNetwork(_ID, *this);
 	}
-};
-
-void Network::setupNetwork() {
-
-}
-
-void Network::setupNetwork(const vector<int>& dimensions) {
-
-};
-
-void Network::loadWeightsFromFile() { //*** TODO ***
-	// I intend to implement this using boost::serialization
-};
-
-void Network::saveWeightsToFile() { 	//***TODO***
-	// I intend to implement this using boost::serialization
 };
 
 double Network::evaluateBoard(vector<char> inputBoard) const { 	//***TODO***
 	double dummy = 0;
 	return dummy;
 };
-
 void Network::adjustPerformance(int resultFromGame) {
 	_performance += resultFromGame;
 	_gameCompleted = true;
@@ -48,34 +44,60 @@ void Network::adjustPerformance(int resultFromGame) {
 int Network::getPerformance() const {
 	return _performance;
 }
-
 void Network::resetPerformance() {
 	_performance = 0;
 };
-
-vector<vector<double>> & Network::evolve() const { 	// *** TODO ***
+vector<Network::weightsInLayer> Network::evolve() const { 	// *** TODO *** Not required for Project 2
 	vector<weightsInLayer> dummy;
 	return dummy;
 };
-
-void Network::replaceWithEvolution(vector<weightsInLayer> & inputNetwork) {
-	swap(_layers, inputNetwork);
+void Network::replaceWithEvolution(const Network & inputNetwork) {
+	_layers = std::move(inputNetwork.evolve());
 };
 
-bool operator<(const Network & lhs, const Network & rhs) {
+bool AI::operator<(const Network & lhs, const Network & rhs) {
 	if (lhs.getPerformance() < rhs.getPerformance())
 		return true;
 	return false;
 }
 
-bool operator>(const Network & lhs, const Network & rhs) {
+bool AI::operator>(const Network & lhs, const Network & rhs) {
 	return rhs < lhs;
 }
 
-bool operator<=(const Network & lhs, const Network & rhs) {
+bool AI::operator<=(const Network & lhs, const Network & rhs) {
 	return !(lhs > rhs);
 }
 
-bool operator>=(const Network & lhs, const Network & rhs) {
+bool AI::operator>=(const Network & lhs, const Network & rhs) {
 	return !(lhs < rhs);
+}
+
+void AI::setupNetwork() {
+
+}
+
+void AI::setupNetwork(const vector<int>& dimensions) {
+
+};
+
+string AI::idToFilename(int ID) {
+	string filename = to_string(ID) + ".network";
+	return filename;
+}
+
+void AI::saveNetwork(int ID, Network & networkToSave) {
+	ofstream file{ idToFilename(ID) };
+	text_oarchive oa{ file };
+	oa << networkToSave;
+}
+bool AI::loadNetwork(int ID, Network & networkRecievingData) {
+	ifstream file{ idToFilename(ID) };
+	if (!file) {
+		std::cout << "The file does not exist" << std::endl;
+		return false;
+	}
+	text_iarchive ia{ file };
+	ia >> networkRecievingData;
+	return true;
 }
