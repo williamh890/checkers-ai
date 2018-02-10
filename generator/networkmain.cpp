bf0c06f9279d7d99348c38f7ece87ed4cacd0506
William Horn <wbhorn@alaska.edu>
2018-02-10 13:23:37 -0900

    Network main is compiling and running for me.
    You will need to add seeder.cpp because it is now using the polymorphic
    random seeder.

--- generator/networkmain.cpp
+++ generator/networkmain.cpp
@@ -3,7 +3,7 @@
 
 //Change this include to your own path to the network.h
 //network.h looks for things locally and so it should be fine to run unaltered.
-#include "C:\Users\Duane\Documents\dlshaffer\checkers-ai\generator\headers\network.h"
+#include "headers/network.h"
 #include <iostream>
 #include <chrono>
 #include <vector>
@@ -19,13 +19,12 @@ int main() {
     vector <unsigned int> networkCreationInput = { 4, 32, 40, 10, 1 };
 
     // Create only one network so that you dont have 100 files running around. Leave off the second parameter defaults to 100
-    AI::setupNetworks(networkCreationInput, 1);
-
+    ai::setupNetworks(networkCreationInput, 1);
 
     //Sample full board
     vector<char> sampleBoard{ 'r','r','r','r','r','r','r','r',' ','r','r','r',' ', ' ','r', ' ',' ', ' ', 'b', ' ','b','b',' ','b','b','b','b','b','b','b','b','b' };
 
-    AI::Network player(0);
+    ai::Network player(0);
     //Proof of success at runtime
     //player.outputDebug();
     std::cout << "Board was evaluated as: " << player.evaluateBoard(sampleBoard) << std::endl;
