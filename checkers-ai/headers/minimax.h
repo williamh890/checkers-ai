658443188dab90ab07deae949b7e72e5bc90b9fa
William Horn <wbhorn@alaska.edu>
2018-02-16 13:29:27 -0900

    Making new files for the my take on the minimax algo.

---
+++ checkers-ai/headers/minimax.h
@@ -0,0 +1,20 @@
+// min_max.h
+
+#ifndef MIN_MAX_H
+#define MIN_MAX_H
+#include "checkers-game.h"
+// ai::CheckersGame
+
+#include <utility>
+// std::make_pair
+// std::pair
+
+#include <vector>
+// std::vector
+
+static const std::vector<char> COLORS = {'r', 'b'};
+namespace ai {
+    int minimax(std::pair<int, int> move, int depth, char maximizingPlayer, CheckersGame & game);
+    int minimax(std::pair<int, Jump> jump, int depth, char maximizingPlayer, CheckersGame & game);
+}
+#endif
