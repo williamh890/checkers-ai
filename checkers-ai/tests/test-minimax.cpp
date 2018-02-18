6e07c80b10fcf8f760443774b2109a206f8f3922
William Horn <wbhorn@alaska.edu>
2018-02-17 15:32:26 -0900

    Base case testing for my version of minimax

--- checkers-ai/tests/test-minimax.cpp
+++ checkers-ai/tests/test-minimax.cpp
@@ -1,4 +1,5 @@
 #include "catch.hpp"
+
 #include "../headers/minimax.h"
 using ai::minimax;
 
@@ -12,14 +13,40 @@ using std::make_pair;
 using std::cout;
 using std::endl;
 
-TEST_CASE("test minimax search funciton") {
+TEST_CASE("test minimax search funciton", "[minimax]") {
     auto game = getCheckersGame();
 
     SECTION("test base case") {
-        cout << "testing base case" << endl;
-        for ( auto & move : game.getValidMoves() ) {
-            REQUIRE(minimax(move, 0, 'r', game) == 12);
-            REQUIRE(minimax(move, 0, 'b', game) == 12);
+        SECTION("testing all base cases from init board") {
+            cout << "testing base case" << endl;
+            for ( auto & move : game.getValidMoves() ) {
+                REQUIRE(minimax(move, 0, 'r', game) == 12);
+                REQUIRE(minimax(move, 0, 'b', game) == 12);
+            }
+        }
+
+        SECTION("testing minimax doesn't change the game state when run") {
+            auto redPieces = game.red->getPieces();
+            auto blackPieces = game.black->getPieces();
+            auto boardState = game.board.getBoardState();
+
+            for ( auto & move : game.getValidMoves() ) {
+                minimax(move, 0, 'r', game);
+
+                REQUIRE(game.red->getPieces() == redPieces);
+                REQUIRE(game.black->getPieces() == blackPieces);
+                REQUIRE(game.board.getBoardState() == boardState);
+
+            }
+
+            for ( auto & move : game.getValidMoves() ) {
+                minimax(move, 0, 'b', game);
+
+                REQUIRE(game.red->getPieces() == redPieces);
+                REQUIRE(game.black->getPieces() == blackPieces);
+                REQUIRE(game.board.getBoardState() == boardState);
+            }
         }
     }
+
 }
