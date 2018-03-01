6bbb42fdd9ed60ae56cbeeaa3e04a2be190e6030
Duane Shaffer <dlshaffer@alaska.edu>
2018-03-01 11:42:15 -0900

    use create_directories to allow for creation of ./networks if needed

--- checkers-ai/networkManagement.cpp
+++ checkers-ai/networkManagement.cpp
@@ -5,7 +5,7 @@ using std::endl;
 #include <vector>
 using std::vector;
 #include <experimental/filesystem>
-using std::experimental::filesystem::create_directory;
+using std::experimental::filesystem::create_directories;
 using std::experimental::filesystem::remove;
 #include <string>
 using std::string;
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
             string username(argv[2]);
             string path = "./networks/" + username;
             cout << "the path I created for your directory was: " << path << endl;
-            create_directory(path) ? cout << "Your directory was created" << endl : 
+            create_directories(path) ? cout << "Your directory was created" << endl : 
                                     (cout << "Couldn't create directory" << endl);
             //remove(path) ? (cout << "I removed it to clean up" << endl) : 
             //                (cout << "Couldn't remove the directory" << endl);
