0c7f23add6157f06e90640a00d33238b31a04469
Duane Shaffer <dlshaffer@alaska.edu>
2018-03-01 11:38:51 -0900

    Creating directories to store networks

--- checkers-ai/networkManagement.cpp
+++ checkers-ai/networkManagement.cpp
@@ -4,6 +4,13 @@ using std::cout;
 using std::endl;
 #include <vector>
 using std::vector;
+#include <experimental/filesystem>
+using std::experimental::filesystem::create_directory;
+using std::experimental::filesystem::remove;
+#include <string>
+using std::string;
+// #include <sstream>
+// using std::istringstream;
 
 int main(int argc, char *argv[]) {
     int returnVal = 0;
@@ -14,6 +21,19 @@ int main(int argc, char *argv[]) {
 
     else if (*argv[1] == '0') {
         cout << "You want me to setup networks." << endl;
+        if (argc <3) {
+            cout << "You didn't give me a username. " << endl;
+            returnVal = -1;
+        }
+        else {
+            string username(argv[2]);
+            string path = "./networks/" + username;
+            cout << "the path I created for your directory was: " << path << endl;
+            create_directory(path) ? cout << "Your directory was created" << endl : 
+                                    (cout << "Couldn't create directory" << endl);
+            //remove(path) ? (cout << "I removed it to clean up" << endl) : 
+            //                (cout << "Couldn't remove the directory" << endl);
+        }
     }
     else if (*argv[1] == '1') {
         cout << "You want me to store performances." << endl;
