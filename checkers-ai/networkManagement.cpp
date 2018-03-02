28c1a74d4171eaaf56105fecf0190188e17f41e1
Duane Shaffer <dlshaffer@alaska.edu>
2018-03-01 15:20:06 -0900

    completed setupNetworks for netMan

--- checkers-ai/networkManagement.cpp
+++ checkers-ai/networkManagement.cpp
@@ -1,4 +1,5 @@
-//#include "headers/network.h"
+#include "headers/network.h"
+using ai::setupNetworks;
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -13,33 +14,35 @@ using std::string;
 // using std::istringstream;
 #include "headers/utils.h"
 using ai::getUsername;
+#include <vector>
+using std::vector;
+
 
 int main(int argc, char *argv[]) {
+    const auto SETUP_NETWORKS = '0';
+    const auto STORE_PERFORMANCES = '1';
+    const auto EVOLVE_NETWORKS = '2';
+
     if (argc == 1) {
         cout << "You called me without any input" << endl;
         return -1;
     }
     string username(getUsername());
-    if (*argv[1] == '0') {
+    if (*argv[1] == SETUP_NETWORKS) {
         cout << "You want me to setup networks." << endl;
-        if (argc <3) {
-            cout << "You didn't give me a username. " << endl;
-            return -1;
-        }
-        else {
-            string path = "./networks/" + username;
-            cout << "the path I created for your directory was: " << path << endl;
-            create_directories(path) ? cout << "Your directory was created" << endl : 
-                                    cout << "Couldn't create directory" << endl;
-            //remove(path) ? cout << "I removed it to clean up" << endl : 
-            //                cout << "Couldn't remove the directory" << endl;
-            
-        }
+        string path = "./networks/" + username;
+        cout << "the path I created for your directory was: " << path << endl;
+        create_directories(path) ? cout << "Your directory was created" << endl : 
+                                cout << "Couldn't create directory (is it already created?)" << endl;
+        //remove(path) ? cout << "I removed it to clean up" << endl : 
+        //                cout << "Couldn't remove the directory" << endl;
+        vector<unsigned int> blondieDimensions{32, 40, 10, 1};
+        setupNetworks(blondieDimensions, 30);
     }
-    else if (*argv[1] == '1') {
+    else if (*argv[1] == STORE_PERFORMANCES) {
         cout << "You want me to store performances." << endl;
     }
-    else if (*argv[1] == '2') {
+    else if (*argv[1] == EVOLVE_NETWORKS) {
         cout << "You want me to evolve your networks" << endl;
     }
     return 0;
