1d90352968f4d33e6428c21b99025634ba157589
William Horn <wbhorn@alaska.edu>
2018-03-21 19:48:15 -0800

    fix build directory creation in install.sh

--- checkers-ai/install.sh
+++ checkers-ai/install.sh
@@ -1,3 +1,4 @@
-mkdir -p $(BUILD_DIR)
+BUILD_DIR=build
+mkdir -p $BUILD_DIR
 curl https://github.com/nlohmann/json/releases/download/v3.1.0/json.hpp > headers/json.hpp -L
 curl https://github.com/catchorg/Catch2/releases/download/v2.1.1/catch.hpp > tests/catch.hpp -L
