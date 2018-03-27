d156ea023ced20e9e8bcee1073f7c2b52fe16c4a
William Horn <wbhorn@alaska.edu>
2018-03-26 22:20:27 -0800

    Improve user experience on setup.py

--- checkers-ai/tournament/setup.py
+++ checkers-ai/tournament/setup.py
@@ -9,11 +9,38 @@ SETUP_NETWORKS = "{} 0".format(NETWORK_MANAGER)
 REPO_URL = "williamh890/checkers-ai-networks.git"
 
 
-def add_generations_cfg(checkers_path, network_path):
-    with open(checkers_path + '/user.me') as f:
-        username = f.read().strip()
+def setup_networks(checkers_path, network_path):
+    while True:
+        network_user = input('  network folder name: ').strip()
 
-    generations_path = os.path.join(network_path, username, 'generations.cfg')
+        if os.path.exists(os.path.join(network_path, network_user)):
+            print("folder name {} already exists in networks directory".format(
+                network_user))
+            continue
+
+        try:
+            path = os.path.join(checkers_path, 'user.me')
+
+            print(path)
+            with open(path, 'w') as f:
+                f.write(network_user)
+        except Exception as e:
+            print(e)
+        else:
+            break
+
+    output = subprocess.getstatusoutput(
+        "cd .. && {} 0".format(NETWORK_MANAGER))
+    print("NETWORK MANAGER")
+    print(output[1])
+    print("DONE")
+    print("")
+
+    generations_path = os.path.join(
+        network_path,
+        network_user,
+        'generations.cfg'
+    )
     if os.path.isdir(generations_path):
         return
 
@@ -31,27 +58,37 @@ if __name__ == "__main__":
     print("Tournament Settings: ")
     print('-' * 40)
 
-    print('networks')
-    new_networks = input("  setup new networks(y/n): ")
-
-    if new_networks == 'y':
-        output = subprocess.getstatusoutput(
-            "cd .. && {} 0".format(NETWORK_MANAGER))
-        print(output[1])
-
     config = SafeConfigParser()
     config['general'] = {'run': 1}
     config['functions'] = {'checkers_game': 'checkers.out',
                            'network_manager': 'manager.out',
                            'network_count': 30}
+
+    checkers_path = '/' + os.path.join(*os.getcwd().split('/')[:-1])
+    network_path = os.path.join(checkers_path, 'networks')
+
     print("absolute path")
-    checkers_path = input("  'checkers-ai' folder: ")
-    network_path = input("  'networks' folder: ")
+    input_checkers_path = input(
+        "  'checkers-ai' folder (default is {}): ".format(checkers_path)
+    )
+    if input_checkers_path != "":
+        print("using default")
+        checker_path = input_checkers_path
+
+    input_network_path = input(
+        "  'networks' folder (default is {}): ".format(network_path))
+
+    if input_network_path != "":
+        network_path = input_network_path
 
     config['paths'] = {'network_path': network_path,
                        'checkers_path': checkers_path}
 
-    add_generations_cfg(checkers_path, network_path)
+    print('networks')
+    new_networks = input("  setup new networks(y/n): ")
+
+    if new_networks == 'y':
+        setup_networks(checkers_path, network_path)
 
     print("git")
     git_username = input("  username: ")
--- checkers-ai/tournament/utils.py
+++ checkers-ai/tournament/utils.py
@@ -2,6 +2,8 @@ from os.path import join, exists, abspath as ap
 
 
 def ensure_generation_cfg(cfg_file):
-    if not(exists(ap(cfg_file))):
-        with open(cfg_file, 'w') as config_file:
-            config_file.write("[generations] \ncount = 0")
+    if exists(ap(cfg_file)):
+        return
+
+    with open(cfg_file, 'w') as config_file:
+        config_file.write("[generations] \ncount = 0")
