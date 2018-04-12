b9a6964b027d5df6a0efd4390a14df1fafa3d337
hadimarchi <hal.dimarchi55@gmail.com>
2018-04-11 19:41:19 -0800

    an empty class

--- checkers-ai/headers/checkers_skynet_interface.h
+++ checkers-ai/headers/checkers_skynet_interface.h
@@ -1,6 +1,14 @@
 #ifndef CHECKERS_SKYNET_INTERFACE_H
 #define CHECKERS_SKYNET_INTERFACE_H
+#include "../../skynet/include/jsoncpp/json.h"
 #include "../../skynet/src/json/json.hpp"
 #include "../../skynet/src/skynet/checkers.hpp"
 #include "../../skynet/src/skynet/checkers_client.hpp"
+#include "checkers-game.h"
+// ai::CheckersGame
+#include "json.hpp"
+
+namespace ai {
+class CheckerSkynetInterface {};
+} // namespace ai
 #endif
---
+++ checkers-ai/src/checkers_skynet_interface.cpp
@@ -0,0 +1,3 @@
+#include "../headers/checkers_skynet_interface.h"
+
+int main() { return 0; }
