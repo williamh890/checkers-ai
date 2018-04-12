c50883ac0de6a0a44f6a377f803e1d86d8f76f74
hadimarchi <hal.dimarchi55@gmail.com>
2018-04-11 19:23:26 -0800

    includes, I have the skynet stuff at the top of our repo

---
+++ checkers-ai/headers/checkers_skynet_interface.h
@@ -0,0 +1,6 @@
+#ifndef CHECKERS_SKYNET_INTERFACE_H
+#define CHECKERS_SKYNET_INTERFACE_H
+#include "../../skynet/src/json/json.hpp"
+#include "../../skynet/src/skynet/checkers.hpp"
+#include "../../skynet/src/skynet/checkers_client.hpp"
+#endif
