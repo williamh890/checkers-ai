f56374f12f8f9a22d552f00f394c13404518f87d
William Horn <wbhorn@alaska.edu>
2018-04-12 16:55:33 -0800

    Fix build errors

--- checkers-ai/src/network.cpp
+++ checkers-ai/src/network.cpp
@@ -219,91 +219,92 @@ Network::evaluateBoard(const vector<char> &inputBoard, bool testing,
   int numPieces = 0;
   int pieceCount = 0;
   for (const auto &ii : inputBoard) {
-    if (ii == ' ' || ii == 0)
-      continue;
+      if (ii == ' ' || ii == 0) {
+          continue;
+      }
 
       ++numPieces;
-    else if (ii == 'r') {
-      pieceCount = pieceCount + red_factor;
-    } else if (ii == 'b') {
-      pieceCount = pieceCount - red_factor;
-    } else if (ii == 'R') {
-      pieceCount = pieceCount + _kingWeight * red_factor;
-    } else if (ii == 'B') {
-      pieceCount = pieceCount - _kingWeight * red_factor;
-    } else {
-      throw "ERROR!!!"
-    }
+      if (ii == 'r') {
+          pieceCount = pieceCount + red_factor;
+      } else if (ii == 'b') {
+          pieceCount = pieceCount - red_factor;
+      } else if (ii == 'R') {
+          pieceCount = pieceCount + _kingWeight * red_factor;
+      } else if (ii == 'B') {
+          pieceCount = pieceCount - _kingWeight * red_factor;
+      } else {
+          throw "ERROR in Network::evaluateBoard!!!";
+      }
   }
   unsigned int layerBeginningIndex = _whichLayerofNetworkToUse[numPieces][0];
   unsigned int layerEndingIndex = _whichLayerofNetworkToUse[numPieces][1];
 
   /*void inputBoardIntoFirstLayer(inputBoard)*/ {
-    int index = 0;
-    for (const auto &i : inputBoard) {
-      if (i == ' ' || i == 0)
-        _layers[layerBeginningIndex][index] = 0;
-      else if (i == 'r')
-        _layers[layerBeginningIndex][index] = 1;
-      else if (i == 'b')
-        _layers[layerBeginningIndex][index] = -1;
-      else if (i == 'R')
-        _layers[layerBeginningIndex][index] = 1 * _kingWeight;
-      else if (i == 'B')
-        _layers[layerBeginningIndex][index] = -1 * _kingWeight;
-      else
-        cout << "Unrecognized character in board: " << i << endl;
-      ++index;
-    }
+      int index = 0;
+      for (const auto &i : inputBoard) {
+          if (i == ' ' || i == 0)
+              _layers[layerBeginningIndex][index] = 0;
+          else if (i == 'r')
+              _layers[layerBeginningIndex][index] = 1;
+          else if (i == 'b')
+              _layers[layerBeginningIndex][index] = -1;
+          else if (i == 'R')
+              _layers[layerBeginningIndex][index] = 1 * _kingWeight;
+          else if (i == 'B')
+              _layers[layerBeginningIndex][index] = -1 * _kingWeight;
+          else
+              cout << "Unrecognized character in board: " << i << endl;
+          ++index;
+      }
   }
   /*void feedForward()*/ {
-    for (unsigned int x = layerBeginningIndex + 1; x < layerEndingIndex; ++x) {
-      for (unsigned int y = 0; y < _layers[x].size(); ++y) {
-        /*calculateNode(x, y)*/ {
-          NetworkWeightType total1 = 0, total2 = 0, total3 = 0, total4 = 0;
-          unsigned int previousLayerSize = _layers[x - 1].size();
+      for (unsigned int x = layerBeginningIndex + 1; x < layerEndingIndex; ++x) {
+          for (unsigned int y = 0; y < _layers[x].size(); ++y) {
+              /*calculateNode(x, y)*/ {
+                  NetworkWeightType total1 = 0, total2 = 0, total3 = 0, total4 = 0;
+                  unsigned int previousLayerSize = _layers[x - 1].size();
 
-          for (unsigned int i = 0; i < previousLayerSize; i += 4) {
-            total1 +=
-                _weights[x][y * previousLayerSize + i] * _layers[x - 1][i];
-            total2 += _weights[x][y * previousLayerSize + i + 1] *
-                      _layers[x - 1][i + 1];
-            total3 += _weights[x][y * previousLayerSize + i + 2] *
-                      _layers[x - 1][i + 2];
-            total4 += _weights[x][y * previousLayerSize + i + 3] *
-                      _layers[x - 1][i + 3];
+                  for (unsigned int i = 0; i < previousLayerSize; i += 4) {
+                      total1 +=
+                          _weights[x][y * previousLayerSize + i] * _layers[x - 1][i];
+                      total2 += _weights[x][y * previousLayerSize + i + 1] *
+                          _layers[x - 1][i + 1];
+                      total3 += _weights[x][y * previousLayerSize + i + 2] *
+                          _layers[x - 1][i + 2];
+                      total4 += _weights[x][y * previousLayerSize + i + 3] *
+                          _layers[x - 1][i + 3];
+                  }
+
+                  auto total = total1 + total2 + total3 + total4;
+
+                  _layers[x][y] = (!testing) ? total / (1 + abs(total)) : total;
+              }
           }
-
-          auto total = total1 + total2 + total3 + total4;
-
-          _layers[x][y] = (!testing) ? total / (1 + abs(total)) : total;
-        }
       }
-    }
   }
   return _layers[layerEndingIndex - 1][0] * red_factor * pieceCount *
-         _pieceCountWeight; /*boardEvaluationOutput()*/
+      _pieceCountWeight; /*boardEvaluationOutput()*/
   ;
 }
 
 inline NetworkWeightType Network::activationFunction(NetworkWeightType x) {
-  // Fast Sigmoid
-  return x / (1 + abs(x));
+    // Fast Sigmoid
+    return x / (1 + abs(x));
 }
 
 void Network::calculateNode(unsigned int x, unsigned int y) {
-  NetworkWeightType totalNodeValue = 0;
-  unsigned int previousLayerSize = _layers[x - 1].size();
+    NetworkWeightType totalNodeValue = 0;
+    unsigned int previousLayerSize = _layers[x - 1].size();
 
-  for (unsigned int i = 0; i < previousLayerSize; i += 2) {
-    totalNodeValue +=
-        _weights[x][y * previousLayerSize + i] * _layers[x - 1][i];
-  }
+    for (unsigned int i = 0; i < previousLayerSize; i += 2) {
+        totalNodeValue +=
+            _weights[x][y * previousLayerSize + i] * _layers[x - 1][i];
+    }
 
-  _layers[x][y] = totalNodeValue;
+    _layers[x][y] = totalNodeValue;
 
-  if (DEBUG)
-    cout << _layers[x][y] << endl;
+    if (DEBUG)
+        cout << _layers[x][y] << endl;
 }
 
 void Network::adjustPerformance(int adjustment) { _performance = adjustment; }
@@ -313,195 +314,195 @@ int Network::getPerformance() const { return _performance; }
 void Network::resetPerformance() { _performance = 0; }
 
 void Network::evolveUsingNetwork(Network &rhs) {
-  _kingWeight = rhs._kingWeight;
-  _weights = rhs._weights;
-  _sigmas = rhs._sigmas;
-  this->evolve();
-  this->resetPerformance();
-  // rhs.resetPerformance();
-  save(_ID, *this);
+    _kingWeight = rhs._kingWeight;
+    _weights = rhs._weights;
+    _sigmas = rhs._sigmas;
+    this->evolve();
+    this->resetPerformance();
+    // rhs.resetPerformance();
+    save(_ID, *this);
 }
 
 void Network::evolve() {
-  evolveKingWeight();
-  evolveSigmas();
-  evolveWeights();
+    evolveKingWeight();
+    evolveSigmas();
+    evolveWeights();
 }
 
 void Network::evolveKingWeight() {
-  uniform_real_distribution<NetworkWeightType> distribution(-1, 1);
-  _kingWeight += distribution(randomNumGenerator);
-  _pieceCountWeight += distribution(randomNumGenerator);
+    uniform_real_distribution<NetworkWeightType> distribution(-1, 1);
+    _kingWeight += distribution(randomNumGenerator);
+    _pieceCountWeight += distribution(randomNumGenerator);
 }
 
 NetworkWeightType Network::getTau() {
-  NetworkWeightType numberofWeights = 0;
+    NetworkWeightType numberofWeights = 0;
 
-  for (unsigned int index = 0; index < _weights.size(); ++index) {
-    numberofWeights += _weights[index].size();
-  }
+    for (unsigned int index = 0; index < _weights.size(); ++index) {
+        numberofWeights += _weights[index].size();
+    }
 
-  NetworkWeightType tau = 1 / (sqrt(2 * sqrt(numberofWeights)));
-  return tau;
+    NetworkWeightType tau = 1 / (sqrt(2 * sqrt(numberofWeights)));
+    return tau;
 }
 
 void Network::evolveSigmas() {
-  auto tau = getTau();
-  for (size_t i = 0; i < _sigmas.size(); ++i) {
-    for (size_t ii = 0; ii < _sigmas[i].size(); ++ii) {
-      evolveSigmaAt(i, ii, tau);
+    auto tau = getTau();
+    for (size_t i = 0; i < _sigmas.size(); ++i) {
+        for (size_t ii = 0; ii < _sigmas[i].size(); ++ii) {
+            evolveSigmaAt(i, ii, tau);
+        }
     }
-  }
 }
 
 void inline Network::evolveSigmaAt(size_t i, size_t ii, NetworkWeightType tau) {
-  _sigmas[i][ii] =
-      _sigmas[i][ii] *
-      exp(tau * getGaussianNumberFromZeroToOne(randomNumGenerator));
+    _sigmas[i][ii] =
+        _sigmas[i][ii] *
+        exp(tau * getGaussianNumberFromZeroToOne(randomNumGenerator));
 }
 
 void Network::evolveWeights() {
-  for (size_t i = 0; i < _weights.size(); ++i) {
-    for (size_t ii = 0; ii < _weights[i].size(); ++ii) {
-      _weights[i][ii] = evolveWeightAt(i, ii);
+    for (size_t i = 0; i < _weights.size(); ++i) {
+        for (size_t ii = 0; ii < _weights[i].size(); ++ii) {
+            _weights[i][ii] = evolveWeightAt(i, ii);
+        }
     }
-  }
 }
 
 NetworkWeightType inline Network::evolveWeightAt(size_t i, size_t ii) {
-  return _weights[i][ii] +
-         _sigmas[i][ii] * getGaussianNumberFromZeroToOne(randomNumGenerator);
+    return _weights[i][ii] +
+        _sigmas[i][ii] * getGaussianNumberFromZeroToOne(randomNumGenerator);
 }
 
 unsigned int Network::getID() const { return _ID; }
 
 void Network::outputCreationDebug() const {
-  cout << "Weight for the king: " << _kingWeight << endl;
-  cout << "Number of layers: " << _layers.size() << endl;
+    cout << "Weight for the king: " << _kingWeight << endl;
+    cout << "Number of layers: " << _layers.size() << endl;
 
-  for (unsigned int index = 0; index < _layers.size(); ++index) {
-    cout << "Size of layer " << index << " = " << _layers[index].size() << endl;
-  }
-
-  cout << "size of weights vector: " << _weights.size() << endl;
-  for (unsigned int index = 0; index < _weights.size(); ++index) {
-    cout << "Size of weight layer " << index << " = " << _weights[index].size()
-         << endl;
-  }
-
-  cout << "_weights data: " << endl;
-  for (auto &v : _weights) {
-    for (auto x : v) {
-      cout << x << " ";
+    for (unsigned int index = 0; index < _layers.size(); ++index) {
+        cout << "Size of layer " << index << " = " << _layers[index].size() << endl;
     }
-    cout << endl;
-  }
 
-  cout << "_sigmas data: " << endl;
-  for (auto &v : _sigmas) {
-    for (auto x : v) {
-      cout << x << " ";
+    cout << "size of weights vector: " << _weights.size() << endl;
+    for (unsigned int index = 0; index < _weights.size(); ++index) {
+        cout << "Size of weight layer " << index << " = " << _weights[index].size()
+            << endl;
     }
-    cout << endl;
-  }
+
+    cout << "_weights data: " << endl;
+    for (auto &v : _weights) {
+        for (auto x : v) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "_sigmas data: " << endl;
+    for (auto &v : _sigmas) {
+        for (auto x : v) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
 }
 
 void Network::changeKingWeight(NetworkWeightType newWeight) {
-  _kingWeight = newWeight;
+    _kingWeight = newWeight;
 }
 
 bool ai::operator<(const Network &lhs, const Network &rhs) {
-  return lhs.getPerformance() < rhs.getPerformance();
+    return lhs.getPerformance() < rhs.getPerformance();
 }
 
 bool ai::operator>(const Network &lhs, const Network &rhs) { return rhs < lhs; }
 
 bool ai::operator<=(const Network &lhs, const Network &rhs) {
-  return !(lhs > rhs);
+    return !(lhs > rhs);
 }
 
 bool ai::operator>=(const Network &lhs, const Network &rhs) {
-  return !(lhs < rhs);
+    return !(lhs < rhs);
 }
 
 bool ai::operator==(const Network &lhs, const Network &rhs) {
-  return (lhs._ID == rhs._ID) && (lhs._kingWeight == rhs._kingWeight) &&
-         (lhs._performance == rhs._performance) &&
-         (lhs._layers == rhs._layers) && (lhs._weights == rhs._weights);
+    return (lhs._ID == rhs._ID) && (lhs._kingWeight == rhs._kingWeight) &&
+        (lhs._performance == rhs._performance) &&
+        (lhs._layers == rhs._layers) && (lhs._weights == rhs._weights);
 }
 
 void ai::setupNetworks(const vector<unsigned int> &dimensions,
-                       int numberOfNetworks) { // numberOfNetworks = 100
-  // cout << "You are about to setup a new set of networks. This operation will
-  // overwrite previous networks. \n" << "Are you sure you want to continue?
-  //(y,n) ";
-  // if (cin.get() == 'n') {
-  // cout << "Not overwriting files" << endl;
-  // cin.ignore();
-  // return;
-  //}
+        int numberOfNetworks) { // numberOfNetworks = 100
+    // cout << "You are about to setup a new set of networks. This operation will
+    // overwrite previous networks. \n" << "Are you sure you want to continue?
+    //(y,n) ";
+    // if (cin.get() == 'n') {
+    // cout << "Not overwriting files" << endl;
+    // cin.ignore();
+    // return;
+    //}
 
-  auto seeder = getSeeder();
-  for (auto index = 0; index < numberOfNetworks; ++index) {
-    Network(index, dimensions, seeder);
-  }
+    auto seeder = getSeeder();
+    for (auto index = 0; index < numberOfNetworks; ++index) {
+        Network(index, dimensions, seeder);
+    }
 
-  // cin.ignore();
+    // cin.ignore();
 }
 
 NetworkWeightType
 ai::getGaussianNumberFromZeroToOne(std::mt19937 &randomNumGenerator) {
-  normal_distribution<NetworkWeightType> distribution(0, 1);
+    normal_distribution<NetworkWeightType> distribution(0, 1);
 
-  return distribution(randomNumGenerator);
+    return distribution(randomNumGenerator);
 }
 
 bool ai::nothingSimilar(const Network &lhs, const Network &rhs) {
-  bool val = true;
-  if (lhs._weights == rhs._weights) {
-    cout << "Weights were the same" << endl;
-    val = false;
-  } else if (lhs._sigmas == rhs._sigmas) {
-    cout << "Sigmas were the same" << endl;
-    val = false;
-  } else if (lhs._ID == rhs._ID) {
-    cout << "ID's were the same" << endl;
-    val = false;
-  }
-  return val;
+    bool val = true;
+    if (lhs._weights == rhs._weights) {
+        cout << "Weights were the same" << endl;
+        val = false;
+    } else if (lhs._sigmas == rhs._sigmas) {
+        cout << "Sigmas were the same" << endl;
+        val = false;
+    } else if (lhs._ID == rhs._ID) {
+        cout << "ID's were the same" << endl;
+        val = false;
+    }
+    return val;
 }
 
 void ai::weightChangeOut(Network parent, Network child) {
-  std::ofstream output;
-  output.open("weight_change_out.txt", ios::out | ios::trunc);
-  for (size_t i = 0; i < parent._weights.size(); i++) {
-    for (size_t j = 0; j < parent._weights[i].size(); j++) {
-      output << (parent._weights[i][j] - child._weights[i][j]) << endl;
+    std::ofstream output;
+    output.open("weight_change_out.txt", ios::out | ios::trunc);
+    for (size_t i = 0; i < parent._weights.size(); i++) {
+        for (size_t j = 0; j < parent._weights[i].size(); j++) {
+            output << (parent._weights[i][j] - child._weights[i][j]) << endl;
+        }
     }
-  }
-  output.close();
+    output.close();
 }
 
 bool ai::validateNetworks() {
-  for (unsigned int i = 0; i < ai::NETWORKPOPSIZE; ++i) {
-    Network net(i);
-    cout << "ID: " << net._ID << "\tNum Layers: " << net._layers.size() << endl;
-    for (unsigned int index = 0; index < net._layers.size(); ++index) {
-      cout << "Size of layer " << index << " = " << net._layers[index].size()
-           << endl;
+    for (unsigned int i = 0; i < ai::NETWORKPOPSIZE; ++i) {
+        Network net(i);
+        cout << "ID: " << net._ID << "\tNum Layers: " << net._layers.size() << endl;
+        for (unsigned int index = 0; index < net._layers.size(); ++index) {
+            cout << "Size of layer " << index << " = " << net._layers[index].size()
+                << endl;
+        }
+        cout << "size of weights vector: " << net._weights.size() << endl;
+        for (unsigned int index = 0; index < net._weights.size(); ++index) {
+            cout << "Size of weight layer " << index << " = "
+                << net._weights[index].size() << endl;
+        }
+        vector<char> emptyBoard(32);
+        vector<char> sampleBigBoard{'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
+            ' ', 'r', 'r', 'r', ' ', ' ', 'r', ' ',
+            ' ', ' ', 'b', ' ', 'b', 'b', ' ', 'b',
+            'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+        cout << "empty: " << net.evaluateBoard(emptyBoard)
+            << "\t big: " << net.evaluateBoard(sampleBigBoard) << endl;
     }
-    cout << "size of weights vector: " << net._weights.size() << endl;
-    for (unsigned int index = 0; index < net._weights.size(); ++index) {
-      cout << "Size of weight layer " << index << " = "
-           << net._weights[index].size() << endl;
-    }
-    vector<char> emptyBoard(32);
-    vector<char> sampleBigBoard{'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
-                                ' ', 'r', 'r', 'r', ' ', ' ', 'r', ' ',
-                                ' ', ' ', 'b', ' ', 'b', 'b', ' ', 'b',
-                                'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
-    cout << "empty: " << net.evaluateBoard(emptyBoard)
-         << "\t big: " << net.evaluateBoard(sampleBigBoard) << endl;
-  }
-  return false;
+    return false;
 }
