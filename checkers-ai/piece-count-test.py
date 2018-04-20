fe45690b5f96337c7123824d8f2ac4d4418005cc
DuaneShaffer <dlshaffer@alaska.edu>
2018-04-20 09:33:46 -0800

    Better output for game results

--- checkers-ai/piece-count-test.py
+++ checkers-ai/piece-count-test.py
@@ -4,7 +4,8 @@ from multiprocessing import Pool
 import subprocess
 
 DEPTH = 4
-NUM_GAMES = 100
+#NUM_GAMES is number of games played as each color
+NUM_GAMES = 50
 
 
 def run_games_vs_piece_count(network_id, games):
@@ -12,40 +13,51 @@ def run_games_vs_piece_count(network_id, games):
 
     out = pool.map_async(
         run_game,
-        [network_id for _ in range(games)]
+        [(network_id, -1) for _ in range(games)]+[(-1, network_id) for _ in range(games)]
     ).get()
 
     wins = 0
-    for game in out:
-        print("winner {}".format(game))
-        if game['winner'] == 0:
-            print(game['winner'])
+    losses = 0
+    for game in out: 
+        winner = game['winner']
+
+        if winner is None:
+            parsedWinner = 'Draw'
+        elif winner != -1:
+            parsedWinner = 'Network'
+        else:
+            parsedWinner = 'Piece Count'
+
+        print("winner is {}".format(parsedWinner))
+        if game['winner'] == network_id:
             wins += 1
+        elif game['winner'] == -1:
+            losses += 1
+    print("Network: {} Piece count: {} Out of {} games".format(wins, losses, games *2))
 
-    print("{} wins vs piece count out of {} games".format(wins, games))
 
-
-def run_game(red_id):
+def run_game(args):
+    red_id, black_id = args
     prog_name = os.path.join(os.getcwd(), "checkers.out")
 
-    piece_count = -1
     run_str = "{} {} {} {}".format(
-        prog_name, red_id, piece_count, DEPTH
+        prog_name, red_id, black_id, DEPTH
     )
 
     winner, program_output = subprocess.getstatusoutput(run_str)
 
     if winner == 139:
+        print("Error")
         print(program_output)
     game_result = {
         1: red_id,
         0: None,
-        255: piece_count
+        255: black_id
     }[winner]
 
     return {
         "red": red_id,
-        "black": piece_count,
+        "black": black_id,
         "winner": game_result
     }
 
@@ -55,5 +67,5 @@ if __name__ == "__main__":
         network_id = sys.argv[1]
     except:
         network_id = 0
-
+    print ("Running Games...")
     run_games_vs_piece_count(network_id, games=NUM_GAMES)
